67e646d853783a8279e86f10abd98201048ce984
Henry <inet@schleichardt.de>
2024-09-29 16:54:12 +0200

    Update Test-IfElse.psm1

--- Test-IfElse.psm1
+++ Test-IfElse.psm1
@@ -1,4 +1,79 @@
-﻿function Test-IfElse {
+
+#region functions
+
+function Get-PSScriptAnalyzerError
+{
+    <#
+        .SYNOPSIS
+            Create DiagnosticRecord
+        .DESCRIPTION
+            Create an output that PSScriptAnalyzer expects as finding.
+        .PARAMETER StartLine
+            StartLine of the finding
+        .PARAMETER EndLine
+            EndLine of the finding
+        .PARAMETER StartColumn
+            StartColumn of the finding
+        .PARAMETER EndColumn
+            EndColumn iof the finding
+        .PARAMETER Correction
+            Proposal to fix the finding
+        .PARAMETER OptionalDescription
+            Optional description to explain the finding
+        .PARAMETER Message
+            Message displayed by PSScriptAnalyzer
+        .PARAMETER Extent
+            Powershell AST extent
+        .LINK
+            https://github.com/PowerShell/PSScriptAnalyzer
+
+    #>
+    param(
+        [int]$StartLine,
+        [int]$EndLine,
+        [int]$StartColumn,
+        [int]$EndColumn,
+        [string]$Correction,
+        [string]$OptionalDescription,
+        [string]$Message,
+        $Extent
+    )
+
+    $objParams = @{
+        TypeName = 'Microsoft.Windows.PowerShell.ScriptAnalyzer.Generic.CorrectionExtent'
+        ArgumentList = $StartLine, $EndLine, $StartColumn,
+                        $EndColumn, $Correction, $OptionalDescription
+    }
+    $correctionExtent = New-Object @objParams
+    $suggestedCorrections = New-Object System.Collections.ObjectModel.Collection[$($objParams.TypeName)]
+    $suggestedCorrections.add($correctionExtent) | Out-Null
+
+    [Microsoft.Windows.Powershell.ScriptAnalyzer.Generic.DiagnosticRecord]@{
+        "Message"              = $Message
+        "Extent"               = $Extent
+        "RuleName"             = "Test-IfElse"
+        "Severity"             = "Warning"
+        "RuleSuppressionID"    = "Test-IfElse"
+        "SuggestedCorrections" = $suggestedCorrections
+    }
+}
+
+[ScriptBlock]$AllAstsPredicate = {
+    param
+    (
+        [System.Management.Automation.Language.Ast]$Ast
+    )
+    [bool]$ReturnValue = $false
+
+    if ($Ast -is [System.Management.Automation.Language.Ast]) {
+        $ReturnValue = $true;
+    }
+    return $ReturnValue
+}
+
+#endregion functions
+
+function Test-IfElse {
     <#
         .SYNOPSIS
             Testing usage of 'else' blocks.
@@ -22,77 +97,7 @@
         [System.Management.Automation.Language.ScriptblockAst]$ScriptblockAst
     )
 
-    begin{
-        [ScriptBlock]$AllAstsPredicate = {
-            param
-            (
-                [System.Management.Automation.Language.Ast]$Ast
-            )
-            [bool]$ReturnValue = $false
-
-            if ($Ast -is [System.Management.Automation.Language.Ast]) {
-                $ReturnValue = $true;
-            }
-            return $ReturnValue
-        }
-
-        function Get-PSScriptAnalyzerError
-        {
-            <#
-                .SYNOPSIS
-                    Create DiagnosticRecord
-                .DESCRIPTION
-                    Create an output that PSScriptAnalyzer expects as finding.
-                .PARAMETER StartLine
-                    StartLine of the finding
-                .PARAMETER EndLine
-                    EndLine of the finding
-                .PARAMETER StartColumn
-                    StartColumn of the finding
-                .PARAMETER EndColumn
-                    EndColumn iof the finding
-                .PARAMETER Correction
-                    Proposal to fix the finding
-                .PARAMETER OptionalDescription
-                    Optional description to explain the finding
-                .PARAMETER Message
-                    Message displayed by PSScriptAnalyzer
-                .PARAMETER Extent
-                    Powershell AST extent
-                .LINK
-                    https://github.com/PowerShell/PSScriptAnalyzer
-
-            #>
-            param(
-                [int]$StartLine,
-                [int]$EndLine,
-                [int]$StartColumn,
-                [int]$EndColumn,
-                [string]$Correction,
-                [string]$OptionalDescription,
-                [string]$Message,
-                $Extent
-            )
-
-            $objParams = @{
-                TypeName = 'Microsoft.Windows.PowerShell.ScriptAnalyzer.Generic.CorrectionExtent'
-                ArgumentList = $StartLine, $EndLine, $StartColumn,
-                                $EndColumn, $Correction, $OptionalDescription
-            }
-            $correctionExtent = New-Object @objParams
-            $suggestedCorrections = New-Object System.Collections.ObjectModel.Collection[$($objParams.TypeName)]
-            $suggestedCorrections.add($correctionExtent) | Out-Null
-
-            [Microsoft.Windows.Powershell.ScriptAnalyzer.Generic.DiagnosticRecord]@{
-                "Message"              = $Message
-                "Extent"               = $Extent
-                "RuleName"             = "Test-IfElse"
-                "Severity"             = "Warning"
-                "RuleSuppressionID"    = "Test-IfElse"
-                "SuggestedCorrections" = $suggestedCorrections
-            }
-        }
-    }
+    begin{}
     process{
         $ScriptblockAst.FindAll($AllAstsPredicate,$true) |
             Where-Object {$null -ne $_.ElseClause} | ForEach-Object {
